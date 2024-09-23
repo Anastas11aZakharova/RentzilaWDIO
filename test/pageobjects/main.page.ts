0c4d4db018ca6cc38aa6194c4654c8ed03b76112
Anastasiia <nastya.zkhrvvvv@gmail.com>
2024-09-23 12:45:00 +0200

    Added authorization tests

--- test/pageobjects/main.page.ts
+++ test/pageobjects/main.page.ts
@@ -98,32 +98,11 @@ class MainPage extends Page {
   public get loginButton() {
     return $('div[class*="NavbarAuthBlock_buttonEnter"]');
   }
-  public get enterButton() {
-    return $("button=Увійти");
-  }
-  public get authorizationFormTitle() {
-    return $('div[data-testid="authorizationTitle"]');
-  }
-  public get emailOrPhoneNumberFieldErrorMessage() {
-    return $('p[class*="CustomReactHookInput_error_message"]');
-  }
-  public get passwordFieldErrorMessage() {
-    return $('p[class*="CustomReactHookInput_error_message"]');
-  }
-  public get emailOrPhoneNumberField() {
-    return $('input[id="email"]');
-  }
-  public get passwordField() {
-    return $('input[id="password"]');
-  }
-  public get authorizationFormCrossButton() {
-    return $('div[data-testid="authClose"]');
-  }
-  public get hiddenPasswordButton() {
-    return $('div[data-testid="reactHookButton"]');
-  }
   public get userIconDropdown() {
     return $('div[class*="AvatarCircle_wrapper"]');
+  }
+  public get emailInUserDropdown() {
+    return $('div[data-testid="email"]');
   }
   public get myProfileItem() {
     return $('div[data-testid="profile"]');
@@ -134,11 +113,8 @@ class MainPage extends Page {
   public get logoutButton() {
     return $('div[data-testid="logout"]');
   }
-  public get incorrectEmailOrPasswordErrorMessage() {
-    return $('div[data-testid="errorMessage"]');
-  }
-  public get incorrectPasswordErrorMessage() {
-    return $('p[class*="CustomReactHookInput_error_message"]');
+  public get submitAnnouncementButton() {
+    return $("a=Подати оголошення");
   }
 
   public open() {
@@ -178,31 +154,9 @@ class MainPage extends Page {
   public async clickOnLogo() {
     await this.logo.click();
   }
-
   public async clickOnLoginButton() {
     await this.loginButton.click();
   }
-
-  public async clickOnEnterButton() {
-    await this.enterButton.click();
-  }
-
-  public async enterEmailInEmailOrPhoneNumberField(email: string) {
-    await this.emailOrPhoneNumberField.setValue(email);
-  }
-
-  public async enterPasswordInPasswordField(email: string) {
-    await this.passwordField.setValue(email);
-  }
-
-  public async clickOnAuthorizationCrossButton() {
-    await this.authorizationFormCrossButton.click();
-  }
-
-  public async clickOnHiddenPasswordButton() {
-    await this.hiddenPasswordButton.click();
-  }
-
   public async clickOnUserIconDropdown() {
     await this.userIconDropdown.click();
   }
@@ -213,6 +167,9 @@ class MainPage extends Page {
 
   public async clickOnLogoutButton() {
     await this.logoutButton.click();
+  }
+  public async clickOnsubmitAnnouncementButton() {
+    await this.submitAnnouncementButton.click();
   }
 }
 
