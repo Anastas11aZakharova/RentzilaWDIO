d72e7ce84ffdc71805faf45681ef64dc8e05b58e
Anastasiia <nastya.zkhrvvvv@gmail.com>
2024-09-26 09:43:46 +0200

    added hiding credentails and initial tests for Adverts page

--- test/pageobjects/page.ts
+++ test/pageobjects/page.ts
@@ -2,6 +2,6 @@ import { browser } from "@wdio/globals";
 
 export default class Page {
   public open(path: string) {
-    return browser.url(`https://dev.rentzila.com.ua/`+path);
+    return browser.url(`/`+path);
   }
 }
--- wdio.conf.ts
+++ wdio.conf.ts
@@ -1,10 +1,21 @@
+import * as dotenv from 'dotenv';
+dotenv.config();
 import type { Options } from '@wdio/types'
 export const config: Options.Testrunner = {
+    baseUrl: process.env.BASE_URL || 'https://defaulturl.com',
     runner: 'local',
     tsConfigPath: './tsconfig.json',
     specs: [
         './test/specs/**/*.ts'
     ],
+    suites: {
+        authorization: [
+            './test/specs/authorization.ts'
+        ],
+        advert: [
+            './test/specs/advert.ts'
+        ]
+    },
     exclude: [
         // 'path/to/excluded/files'
     ],
