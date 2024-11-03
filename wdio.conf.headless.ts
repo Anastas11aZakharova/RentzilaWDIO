a62e37dc93dbe191cb8f9c968e0781908a06d779
Anastasiia <nastya.zkhrvvvv@gmail.com>
2024-11-03 22:55:47 +0100

    Change according to review comments

--- wdio.conf.headless.ts
+++ wdio.conf.headless.ts
@@ -1,5 +1,5 @@
 import { config  } from "./wdio.conf";
 
-config.capabilities[0]['goog:chromeOptions'].args = ['headless', 'disable-gpu'];
+config.capabilities[0]['goog:chromeOptions'].args = ['headless', 'disable-gpu', '--window-size=1920,1080'];
 
 exports.config = config;
